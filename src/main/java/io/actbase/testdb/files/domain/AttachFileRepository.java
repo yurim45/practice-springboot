bc14ea553d0f4e4e8546d9bc1a2d05bcbc6d1208
yrkim <icho0405@gmail.com>
2021-10-27 08:26:40 +0900

    에러파일 주석처리

--- src/main/java/io/actbase/testdb/files/domain/AttachFileRepository.java
+++ src/main/java/io/actbase/testdb/files/domain/AttachFileRepository.java
@@ -1,15 +1,15 @@
-//package io.actbase.testdb.files.domain;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-//
-///**
-// * The interface Attach file repository.
-// *
-// * @author eomjeongjae
-// * @since 2019 /10/15
-// */
-//public interface AttachFileRepository extends JpaRepository<AttachFile, Long>,
-//        JpaSpecificationExecutor<AttachFile> {
-//
-//}
+// package io.actbase.testdb.files.domain;
+
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+// /**
+//  * The interface Attach file repository.
+//  *
+//  * @author eomjeongjae
+//  * @since 2019 /10/15
+//  */
+// public interface AttachFileRepository extends JpaRepository<AttachFile, Long>,
+//         JpaSpecificationExecutor<AttachFile> {
+
+// }
--- src/main/java/io/actbase/testdb/files/properties/FileProperties.java
+++ src/main/java/io/actbase/testdb/files/properties/FileProperties.java
@@ -1,27 +1,27 @@
-//package io.actbase.testdb.files.properties;
-//
-//import lombok.Getter;
-//import lombok.Setter;
-//import lombok.ToString;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.stereotype.Component;
-//
-///**
-// * The type File properties.
-// *
-// * @author eomjeongjae
-// * @since 04 /10/2019
-// */
-//@Getter
-//@Setter
-//@ToString
-//@Component
-//@ConfigurationProperties("file")
-//public class FileProperties {
-//
-//  private String fileRedirect;
-//  private String xSendFile;
-//  private String extensionHeic;
-//  private String extensionConvert;
-//  private String extensionComposite;
-//}
+// package io.actbase.testdb.files.properties;
+
+// import lombok.Getter;
+// import lombok.Setter;
+// import lombok.ToString;
+// import org.springframework.boot.context.properties.ConfigurationProperties;
+// import org.springframework.stereotype.Component;
+
+// /**
+//  * The type File properties.
+//  *
+//  * @author eomjeongjae
+//  * @since 04 /10/2019
+//  */
+// @Getter
+// @Setter
+// @ToString
+// @Component
+// @ConfigurationProperties("file")
+// public class FileProperties {
+
+//   private String fileRedirect;
+//   private String xSendFile;
+//   private String extensionHeic;
+//   private String extensionConvert;
+//   private String extensionComposite;
+// }
